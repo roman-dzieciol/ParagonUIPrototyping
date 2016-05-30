a049e465c4927fafb918088d62bf4c6804b6a07c
Roman Dzięcioł <roman.dzieciol@gmail.com>
2016-05-30 13:34:55 +0200

    Add Blueprint nodes for playing on hover animations in reverse from current state

--- Source/DeckBuilderDesktop/DesignableUserWidget.cpp
+++ Source/DeckBuilderDesktop/DesignableUserWidget.cpp
@@ -2,6 +2,8 @@
 
 #include "DeckBuilderDesktop.h"
 #include "DesignableUserWidget.h"
+#include "UMGSequencePlayer.h"
+#include "WidgetAnimation.h"
 
 void UDesignableUserWidget::OnSynchronizeProperties_Implementation() 
 {
@@ -10,4 +12,37 @@ void UDesignableUserWidget::OnSynchronizeProperties_Implementation()
 void UDesignableUserWidget::SynchronizeProperties() {
 	Super::SynchronizeProperties();
 	OnSynchronizeProperties();
+}
+
+
+bool UDesignableUserWidget::GetAnimationTimeCursorPosition(const UWidgetAnimation* Animation, float& OutTimeCursorPosition) const
+{
+	if (Animation)
+	{
+		UUMGSequencePlayer* const* FoundPlayer = ActiveSequencePlayers.FindByPredicate(
+			[&](const UUMGSequencePlayer* Player)
+		{
+			return Player->GetAnimation() == Animation;
+		});
+
+		if (FoundPlayer)
+		{
+			OutTimeCursorPosition = (*FoundPlayer)->GetTimeCursorPosition();
+			return true;
+		}
+	}
+	return false;
+}
+
+float UDesignableUserWidget::GetAnimationCurrentStartTimeForPlayMode(const UWidgetAnimation* Animation, float DefaultStartTime, EUMGSequencePlayMode::Type PlayMode) const
+{
+	float TimeCursorPosition = DefaultStartTime;
+	if (Animation && GetAnimationTimeCursorPosition(Animation, TimeCursorPosition))
+	{
+		if (EUMGSequencePlayMode::Reverse == PlayMode)
+		{
+			TimeCursorPosition = (Animation->GetEndTime() - Animation->GetStartTime()) - TimeCursorPosition;
+		}
+	}
+	return TimeCursorPosition;
 }
--- Source/DeckBuilderDesktop/DesignableUserWidget.h
+++ Source/DeckBuilderDesktop/DesignableUserWidget.h
@@ -22,6 +22,26 @@ public:
 	UFUNCTION(BlueprintNativeEvent, Category = "User Interface")
 		void OnSynchronizeProperties();
 
+	/**
+	* Gets current time cursor position for a playing animation
+	*
+	* @param Animation The animation currently being played
+	* @return Current time cursor position for a playing animation
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category = "User Interface|Animation")
+		bool GetAnimationTimeCursorPosition(const UWidgetAnimation* Animation, float& OutTimeCursorPosition) const;
+
+	/**
+	* Gets time cursor position for continuing playing animation from current state (i.e. reverse on hover animation)
+	*
+	* @param Animation The animation currently being played
+	* @param DefaultStartTime The time cursor position to return if animation isn't playing currently
+	* @param PlayMode The play mode to calculate start time for.
+	* @return Time cursor position for continuing playing animation from current state
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintCosmetic, Category = "User Interface|Animation")
+		float GetAnimationCurrentStartTimeForPlayMode(const UWidgetAnimation* Animation, float DefaultStartTime, EUMGSequencePlayMode::Type PlayMode) const;
+
 public:
 	//~ Begin UWidget Interface
 	virtual void SynchronizeProperties() override;
